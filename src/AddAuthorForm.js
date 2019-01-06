10b566d68a80801ddaba16a96dff6196a4549374
mareann <majgfi@gmail.com>
2019-01-06 13:53:41 -0600

    add AuthorForm.js .css

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import "./AddAuthorForm.css";
 
 function AddAuthorForm({match}) {
-	return <div> 
+	return <div className="AddAuthorForm"> 
 	  <h1>Add Author</h1>
-	  <p>{JSON.stringify(match)}</p>
+	  <form>
+	     <div className="AddAuthorForm_input">
+	        <label htmlFor="name">Name</label>
+	        <input type="text" name="name"></input>
+	     </div>
+	     <div className="AddAuthorForm_input">
+	        <label htmlFor="imageUrl">Image URL</label>
+	        <input type="text" name="imageUrl"></input>
+	     </div>
+	  </form>
 	</div>;
 }
 
